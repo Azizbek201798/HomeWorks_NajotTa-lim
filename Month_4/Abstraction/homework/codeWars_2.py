5cfef20d3d253c317b58785555684f2ec93d1e0a
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-04-14 20:02:35 +0500

    HW

---
+++ Month_4/Abstraction/homework/codeWars_2.py
@@ -0,0 +1,12 @@
+import math
+import os
+os.system("clear")
+
+def cooking_time(needed_power, minutes, seconds, power) -> str:
+    total = math.ceil((int(needed_power[:-1]) / int(power[:-1])) * (minutes * 60 + seconds))
+    return f"{(total // 60)} minutes {(total) % 60} seconds"
+
+print(cooking_time("600W",4,20,"800W"))
+print(cooking_time("83W",61,80,"26W"))
+print(cooking_time("7500W",0,5,"600W"))
+print(cooking_time("21W",64,88,"25W"))
