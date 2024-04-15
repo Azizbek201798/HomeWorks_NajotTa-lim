f77b1fe8b54966f665291fb1bb49601edb8d3527
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-04-15 13:31:00 +0500

    HW

---
+++ Month_4/Abstraction/classwork/essential.py
@@ -0,0 +1,55 @@
+# Encapsulation
+    # public data
+    # protected data
+    # private data
+
+# class Dog:
+#     def __init__(self, laqab):
+#         self.__laqab = laqab
+#         # print(self.__laqab)
+
+#     # getter
+#     def get_laqab(self):
+#         return self.__laqab
+    
+#     # setter
+#     def set_laqab(self,data):
+#         self.__laqab = data
+
+# d1 = Dog("Reks")
+# d1.set_laqab("Qoplon")
+
+# print(d1.get_laqab())
+
+
+
+# class Ota:
+#     def __init__(self):
+#         self.__name = "Ilhom"
+    
+#     def get(self):
+#         print(self.__name)
+
+# class Bola(Ota):
+#     def get_name(self):
+#         print(self.__name)
+
+# b1 = Bola()
+# # print(b1.__name)
+# # b1.get_name()
+# b1.get()
+
+
+
+
+# class Joja:
+#     def salom(self):
+#         print("salom")
+#         self.__kim()
+    
+#     def __kim(self):
+#         print("Foundation")
+
+# j1 = Joja()
+# j1.__kim()
+# j1.salom()
